6f0b79cc65df64198029f6f43df90f6069afd6a6
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-14 11:22:58 +0200

    add keyword extraction

--- wiki_api.py
+++ wiki_api.py
@@ -1,4 +1,8 @@
 import requests
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
 
 wiki_api = "https://en.wikipedia.org/api/rest_v1/"
 
@@ -9,9 +13,20 @@ def get_article_from_page(page):
             "description": page['description'], 
             "short": page['extract']
         }, 
-        "id": page['titles']['canonical'] 
+        "id": page['titles']['canonical'],
+        "keywords": get_keywords(page['titles']['canonical'])
     }
 
+def get_keywords(title):
+    S = requests.Session()
+    URL = f"{wiki_api}page/html/{title}"
+    R = S.get(url=URL)
+    S.close()
+    
+    parsed_html = BeautifulSoup(R.text, 'html.parser')
+    links = parsed_html.body.find_all('a', attrs={'rel': 'mw:WikiLink'})
+    keywords = list(set([a.text for a in links]))
+    return keywords
 
 def get_wiki(title):
     S = requests.Session()
@@ -31,17 +46,25 @@ def get_random_wiki():
 
 
 if __name__ == '__main__':
-    # Example: Random Article
+    # Example: Random Article - Summary
     print("######")
     print("Get random article from Wikipedia.en ...")
     article = get_random_wiki()
     print(article)
     print("######")
 
-    # Example: Hello World Article
+    # Example: Hello World Article - Summary
     print()
     print("######")
     print("Get article from Wikipedia.en by Title ['""Hello,_World!""_program'] ...")
     article = get_wiki('"Hello,_World!"_program')
+    print(article)
+    print("######")
+
+     # Example: Hello World Article - Wikitext
+    print()
+    print("######")
+    print("Get article from Wikipedia.en by Title ['""Hello,_World!""_program'] ...")
+    article = get_keywords('"Hello,_World!"_program')
     print(article)
     print("######")
