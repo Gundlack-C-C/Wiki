fb149d042a4c05b881278521d31fccc3ed5f0097
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-14 11:26:22 +0200

    add test for keywords

--- wiki_api_test.py
+++ wiki_api_test.py
@@ -1,10 +1,11 @@
 import unittest
-from wiki_api import get_random_wiki, get_wiki
+from wiki_api import get_random_wiki, get_wiki, get_keywords
 
 class WikiTest(unittest.TestCase):
     def assertValidArticleFormat(self, data):
         self.assertTrue("text" in data, "Missing field: ['text']")
         self.assertTrue("id" in data, "Missing field ['id']")
+        self.assertTrue("keywords" in data, "Missing field ['keywords']")
 
 class TestGetRandom(WikiTest):
     def test_random(self):
@@ -17,6 +18,13 @@ class TestGet(WikiTest):
         res = get_wiki('"Hello,_World!"_program')
         self.assertIsNotNone(res, "Empty response")
         self.assertValidArticleFormat(res)
+
+class TestKeywords(WikiTest):
+    def test_get_keywords(self):
+        res = get_keywords('"Hello,_World!"_program')
+        self.assertIsNotNone(res, "Empty response")
+        self.assertIsInstance(res, list)
+        self.assertEqual(len(list(set(res))), len(res))
 
 if __name__ == '__main__':
     unittest.main()
