e8cd338f28f84729c0565c4e5795b10b4b901f41
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-14 10:33:14 +0200

    refactor to return same format article

--- wiki_api.py
+++ wiki_api.py
@@ -2,18 +2,28 @@ import requests
 
 wiki_api = "https://en.wikipedia.org/api/rest_v1/"
 
+def get_article_from_page(page):
+    return {
+        "text": {
+            "title": page['title'], 
+            "description": page['description'], 
+            "short": page['extract']
+        }, 
+        "id": page['titles']['canonical'] 
+    }
+
 def get_wiki(title):
     S = requests.Session()
     URL = f"{wiki_api}page/summary/{title}"
     R = S.get(url=URL)
     page = R.json()
-
-    return f"{page['title']}. {page['extract']}"
+    return get_article_from_page(page)
 
 def get_random_wiki():
     S = requests.Session()
     URL = f"{wiki_api}page/random/summary"
     R = S.get(url=URL)
     page = R.json()
+    return get_article_from_page(page)
 
-    return f"{page['title']}. {page['extract']}"
+
